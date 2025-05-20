860bff57eae11c94113a283243c97e0a383eea57
Ray <ray@example.com>
2025-05-20 16:23:19 +0800

    在這個 Helm 值檔案中，您可以看到一些 Keycloak 的配置。其中一部分是關於 PostgreSQL 的配置，另一部分則是 Keycloak 自身的配置。
    為了簡單化說明，我會將它們分成兩個部分進行解說：
    
    1. **PostgreSQL 配置**：這部分會根據您設定的參數啟用或禁用 PostgreSQL Helm Chart。如果 PostgreSQL 已經存在，您可以提供密碼、使用者名稱、庫名等屬性來建立自己的資料庫。
    
    2. **Keycloak 配置**：這部分包含了 Keycloak 的配置，例如 Cache 和 Logging 的設定。您可以在此選擇是否啟用 Keycloak 的分散式缓存，以及是使用哪種 Cache Stack，還有其他一些相關的配置項。
    
    如果需要深入了解這些配置項，請參考相應的官方文件：
    - PostgreSQL Helm Chart: <https://github.com/bitnami/charts/tree/main/bitnami/postgresql>
    - Keycloak Configuration: <https://www.keycloak.org/server/all-config/>

--- keycloak/helm.sh
+++ keycloak/helm.sh
@@ -1,2 +1,2 @@
 # https://artifacthub.io/packages/helm/bitnami/keycloak
-helm upgrade --install -n keycloak-system --create-namespace keycloak oci://registry-1.docker.io/bitnamicharts/keycloak
+helm upgrade -f values.yaml --install -n keycloak-system --create-namespace keycloak oci://registry-1.docker.io/bitnamicharts/keycloak
---
+++ keycloak/olm/README.md
@@ -0,0 +1,4 @@
+# keycloak OLM
+
+OLM 版本可以在難用一點沒關係 ^^  
+連 https 都不支援...
